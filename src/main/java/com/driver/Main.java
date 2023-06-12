a2c8d129ddcfe3c5aa04a82a9c8abecc5b07fc4c
Anoop kushwaha <anoopkushwaha509@gmail.com>
2023-06-12 15:54:59 -0400

    Assignment to understand polymorphism -1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,23 +2,23 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        product p = new product();
-        int a= p.product(5, 6);
-        int b = p.product(1,2,3);
-        double c= p.product(1.1,2.1);
+        Product p = new Product();
+        int a= p.Product(5, 6);
+        int b = p.Product(1,2,3);
+        double c= p.Product(1.1,2.1);
     }
-    public static class product{
+    public static class Product{
 
-        public int product(int x, int y) {
+        public int Product(int x, int y) {
 
             return  x*y;
 
         }
-        public int product(int x, int y, int z) {
+        public int Product(int x, int y, int z) {
             return  x*y*z;
         }
 
-        public double product(double x, double y) {
+        public double Product(double x, double y) {
 
 
             return x*y;
